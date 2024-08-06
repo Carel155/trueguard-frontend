9bb35fbfdcd0b9a573bf32388e90aaff1cd9fe5d
carel155 <carelmartten@gmail.com>
2024-08-06 22:19:59 +0300

    Fix env

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <html lang="en">
           <body className={inter.className}>
             <Tracking />
-            {process.env.NEXT_PUBLIC_ENV !== "production" && (
+            {process.env.NEXT_PUBLIC_ENV === "production" && (
               <Script id="reddit-pixel">
                 {`!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_ffz9z2hbrd9k');rdt('track', 'PageVisit');`}
               </Script>
