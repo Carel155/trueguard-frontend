e0198e2d40288de3ca2491b1dcd5a2642dfdf68e
carel155 <carelmartten@gmail.com>
2024-08-06 22:05:14 +0300

    Add reddit pixel

--- components/tracking.tsx
+++ components/tracking.tsx
@@ -3,6 +3,8 @@
 import { useEffect } from "react";
 import ReactGA from "react-ga4";
 import { clarity } from "react-microsoft-clarity";
+// @ts-ignore
+import RedditPixel from "react-reddit-pixel";
 
 const Tracking = () => {
   const initializeTracking = () => {
@@ -11,7 +13,11 @@ const Tracking = () => {
     }
 
     ReactGA.initialize([{ trackingId: "G-84NHYJ10KR" }]);
+    ReactGA.event;
     clarity.init("nj6mnc82ng");
+
+    RedditPixel.init("a2_ffz9z2hbrd9k");
+    RedditPixel.pageVisit();
   };
 
   useEffect(() => {
